fcbc543c489bd9b4b612cbafae07f3ba1a798039
Ammarx9 <ammarxx919@gmail.com>
2024-10-17 10:05:38 +0300

    fix thing

--- packages/all_pkg/get_all_pkg/lib/get_all_pkg.dart
+++ packages/all_pkg/get_all_pkg/lib/get_all_pkg.dart
@@ -1,13 +1,13 @@
 library get_all_pkg;
 
-import 'package:sizer/sizer.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:get_it/get_it.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:icons_plus/icons_plus.dart';
+export 'package:sizer/sizer.dart';
+export 'package:flutter_dotenv/flutter_dotenv.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:carousel_slider/carousel_slider.dart';
+export 'package:get_storage/get_storage.dart';
+export 'package:get_it/get_it.dart';
+export 'package:google_fonts/google_fonts.dart';
+export  'package:icons_plus/icons_plus.dart';
 
 
 
