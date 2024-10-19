0a8fe012a1e2f7174849e0a771aa0effcc7b8abe
Ammarx9 <ammarxx919@gmail.com>
2024-10-20 01:43:11 +0300

    makeing food model and child model

--- packages/database_meth/lib/database/method/child_mix.dart
+++ packages/database_meth/lib/database/method/child_mix.dart
@@ -4,30 +4,27 @@ import 'package:database_meth/database/super_main.dart';
 
 mixin ChildMix {
   addChild(
-    {
-      required String name,
+      {required String name,
       required String userId,
       required List<String> allergy,
-      required String clas ,
-      required  String imgPath,
+      required String clas,
+      required String imgPath,
       required String schoolId,
-      required double funds
-
-
-    }
-  ) async {
+      required double funds}) async {
     try {
-     await  SuperMain().supabase.from("followers").insert({
-
-        "name" : name,
-        "user_id": userId ,
+      await SuperMain().supabase.from("followers").insert({
+        "name": name,
+        "user_id": userId,
         "funds": funds,
-        "allergy" : allergy,
-        "class"  : clas,
-        "img_path" : imgPath,
-        "school_id" :schoolId,
-
+        "allergy": allergy,
+        "class": clas,
+        "img_path": imgPath,
+        "school_id": schoolId,
       });
+
+     await SuperMain().supabase.rpc('increment_followers', params: {
+  'user_id': userId, 
+});
     } catch (er) {
       log("$er");
     }
---
+++ packages/get_all_pkg/lib/data/model/child_model.dart
@@ -0,0 +1,47 @@
+class ChildModel {
+  final String id;
+  final String name;
+  final String userId;
+  final String schoolId;
+  final String imgPath;
+  final String studentClass;
+  final List<String> allergy;
+  final num funds;
+
+  ChildModel({
+    required this.id,
+    required this.name,
+    required this.userId,
+    required this.schoolId,
+    required this.imgPath,
+    required this.studentClass,
+    required this.allergy,
+    required this.funds,
+  });
+
+  factory ChildModel.fromJson(Map<String, dynamic> json) {
+    return ChildModel(
+      id: json['id'],
+      name: json['name'],
+      userId: json['user_id'],
+      schoolId: json['school_id'],
+      imgPath: json['img_path'],
+      studentClass: json['class'],
+      allergy: List<String>.from(json['allergy']),
+      funds: json['funds'].toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'user_id': userId,
+      'school_id': schoolId,
+      'img_path': imgPath,
+      'class': studentClass,
+      'allergy': allergy,
+      'funds': funds,
+    };
+  }
+}
