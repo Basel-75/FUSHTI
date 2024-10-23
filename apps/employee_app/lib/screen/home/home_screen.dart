0aeb0975219a7b066c526e2b0b6ee3df882fcc4f
Anas Aljuhani <Anas.aljuhani.dev@gmail.com>
2024-10-23 07:17:38 +0300

    add product ui done.

--- apps/employee_app/lib/screen/home/home_screen.dart
+++ apps/employee_app/lib/screen/home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:developer';
 
 import 'package:employee_app/widget/appbar_emp_header.dart';
+import 'package:employee_app/widget/cardProduct/card_prodct_widget.dart';
+import 'package:employee_app/widget/textTitle/title_name.dart';
 import 'package:flutter/material.dart';
 import 'package:get_all_pkg/get_all_pkg.dart';
 
@@ -83,9 +85,10 @@ class HomeScreen extends StatelessWidget {
           child: AppBar(
             leading: IconButton(
                 onPressed: () {}, icon: const Icon(Icons.chat_outlined)),
-            flexibleSpace: const EmployeeHeader(
+            flexibleSpace: EmployeeHeader(
               isTitle: false,
               title: 'اضافة منتج',
+              textSize: 20.sp,
             ),
           ),
         ),
@@ -139,178 +142,6 @@ class HomeScreen extends StatelessWidget {
               ),
             )
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class ProductWidgets extends StatelessWidget {
-  const ProductWidgets({
-    super.key,
-    // required this.products,
-    // required this.product,
-    this.onTap,
-    this.editButton,
-    this.deleteButton,
-    required this.image,
-    required this.name,
-    required this.rating,
-    required this.calories,
-    required this.price,
-    required this.sizeBorder,
-    required this.borderColor,
-  });
-
-  // final List<Map<String, dynamic>> products;
-  // final Map<String, dynamic> product;
-  final void Function()? onTap;
-  final void Function()? editButton;
-  final void Function()? deleteButton;
-  final String image;
-  final String name;
-  final String rating;
-  final int calories;
-  final int price;
-  final double sizeBorder;
-  final Color borderColor;
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        padding: EdgeInsets.all(0.8.h),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15.0),
-          border: Border.all(width: sizeBorder, color: borderColor),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              spreadRadius: 2,
-              blurRadius: 5,
-              offset: const Offset(0, 3),
-            ),
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InkWell(
-                  onTap: deleteButton,
-                  child: Icon(Icons.disabled_by_default_outlined,
-                      size: 20.sp, color: Colors.red),
-                ),
-                InkWell(
-                  onTap: editButton,
-                  child: Icon(Icons.edit, size: 20.sp, color: Colors.grey),
-                ),
-              ],
-            ),
-            Image.asset(
-              image,
-              height: 8.5.h,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                      overflow: TextOverflow.ellipsis),
-                ),
-                Row(
-                  children: [
-                    const Icon(Icons.star, color: Colors.amber, size: 18),
-                    Text(
-                      rating,
-                      style: TextStyle(
-                        fontSize: 14.5.sp,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.amber,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 1.h,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '${calories} سعرة',
-                  style: TextStyle(fontSize: 15.sp, color: Colors.grey),
-                ),
-                Text(
-                  '${price} رس',
-                  style: TextStyle(
-                    fontSize: 15.sp,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class EmpCategoryTitle extends StatelessWidget {
-  const EmpCategoryTitle({
-    super.key,
-    required this.nameCategory,
-    required this.textSize,
-  });
-  final String nameCategory;
-  final double textSize;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(right: 4.h),
-      child: Align(
-        alignment: Alignment.centerRight,
-        child: Text(
-          nameCategory,
-          style: TextStyle(fontSize: textSize, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
-  }
-}
-
-class EmpTitleName extends StatelessWidget {
-  const EmpTitleName({
-    super.key,
-    required this.schoolName,
-    required this.textSize,
-    required this.paddingRight,
-    required this.paddingTop,
-  });
-  final String schoolName;
-  final double textSize;
-  final double paddingRight;
-  final double paddingTop;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(right: paddingRight, top: paddingTop),
-      child: Align(
-        alignment: Alignment.centerRight,
-        child: Text(
-          schoolName,
-          style: TextStyle(fontSize: textSize, fontWeight: FontWeight.bold),
         ),
       ),
     );
