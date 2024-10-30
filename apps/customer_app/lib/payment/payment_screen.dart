db4f68cd2109f9e11de716b8d7b60b941ed9b83a
Anas Aljuhani <Anas.aljuhani.dev@gmail.com>
2024-10-30 14:34:56 +0300

    some edit

--- apps/customer_app/lib/payment/payment_screen.dart
+++ apps/customer_app/lib/payment/payment_screen.dart
@@ -1,23 +1,23 @@
-import 'package:flutter/material.dart';
-import 'package:get_all_pkg/get_all_pkg.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get_all_pkg/get_all_pkg.dart';
 
-class PaymentScreen extends StatelessWidget {
-  const PaymentScreen({super.key, required this.paymentConfig});
-  final PaymentConfig paymentConfig;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            CreditCard(
-                config: paymentConfig,
-                onPaymentResult: (PaymentResponse paymentResponse) {
-                  Navigator.of(context).pop(paymentResponse);
-                })
-          ],
-        ),
-      ),
-    );
-  }
-}
+// class PaymentScreen extends StatelessWidget {
+//   const PaymentScreen({super.key, required this.paymentConfig});
+//   final PaymentConfig paymentConfig;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SingleChildScrollView(
+//         child: Column(
+//           children: [
+//             CreditCard(
+//                 config: paymentConfig,
+//                 onPaymentResult: (PaymentResponse paymentResponse) {
+//                   Navigator.of(context).pop(paymentResponse);
+//                 })
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
